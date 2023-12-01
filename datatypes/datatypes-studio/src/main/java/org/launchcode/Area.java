82c76bcf71cec69134da4893bfa369455442e1b6
ArielleWaks <ariellemwaks@gmail.com>
2023-11-30 20:10:12 -0600

    bonus mission user input validation

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -8,17 +8,47 @@ public class Area {
 
     public static void main(String[] args) {
         double radius;
-        double area;
+//        double area;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a radius");
         radius=input.nextDouble();
-        input.close();
+
+        while (radius < 0) {
+            System.out.println("Enter a positive number");
+            radius=input.nextDouble();
+        }
 
 //        area = 3.14*radius*radius;
-        area = Circle.getArea(radius);
+        double area = Circle.getArea(radius);
 
         System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
+
+//        if (input.hasNextDouble()) {
+//            radius=input.nextDouble();
+//            area = Circle.getArea(radius);
+//            if (radius<0) {
+//                System.out.println("Enter a positive number");
+//                radius=input.nextDouble();
+//            }
+//            System.out.println("The area of a circle of radius " + radius + " is: " + area);
+//            input.close();
+//        } else {
+//            System.out.println("Error: invalid input");
+//            input.close();
+//        }
+
+
+
+        //        do {
+//            System.out.println("Enter a radius");
+//            radius=input.nextDouble();
+//        } while (radius < 0);
+
+
+
+
 
     }
 

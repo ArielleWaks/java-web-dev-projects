21b4de989c7e6128d15e78ef53e54657a484183a
ArielleWaks <ariellemwaks@gmail.com>
2023-12-11 18:40:51 -0600

    classes 2 studio worked on and bonus mission completed

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,2 +1,58 @@
-package org.launchcode;public class Menu {
+package org.launchcode;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private ArrayList<MenuItem> items;
+
+    private Date updated;
+
+    public Menu(Date aUpdated, ArrayList<MenuItem> aItems) {
+        this.updated = aUpdated;
+        this.items = aItems;
+    }
+
+    public String toString() {
+        return "Menu\n" + "updated " + updated + "\n" + items;
+    }
+
+    public void printSimpleMenu() {
+        System.out.println("Menu");
+        for (MenuItem i : items) {
+            System.out.println(i);
+        }
+        System.out.println("Last Updated " + updated);
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+    public void addItem(MenuItem item) {
+        if (items.contains(item)) {
+            return;
+        }
+        items.add(item);
+    }
+
+    public void removeItem(MenuItem item) {
+        if (items.contains(item)) {
+            items.remove(item);
+            //TODO: print menu
+        }
+    }
+
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,2 +1,61 @@
-package org.launchcode;public class MenuItem {
+package org.launchcode;
+
+//import java.time.LocalDate;
+import java.util.Date;
+
+public class MenuItem {
+    //private String name;
+    private double price;
+    private String description;
+    private String category;
+
+    private final Date created;
+
+    //private LocalDate date;
+    //private boolean isNew;
+
+    public MenuItem(double aPrice, String aDescription, String aCategory, Date aCreated) {
+        this.price = aPrice;
+        this.description = aDescription;
+        this.category = aCategory;
+        this.created = aCreated;
+        //this.isNew = aIsNew;
+        //this.name = name;
+        //this.date = LocalDate.now();
+    }
+
+    //public String getName() { return name; }
+
+    public double getPrice() { return price; }
+
+    public void setPrice(double aPrice) {
+        price = aPrice;
+    }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String aDescription) {
+        this.description = aDescription;
+    }
+
+    public String getCategory()  { return category; }
+
+    public void setCategory(String aCategory) {
+        aCategory = this.category;
+    }
+
+//    public boolean isNew(Date today) {
+//        public int compareTo;
+//        return;
+//    }
+
+//    public LocalDate getDate() {
+//        return date;
+//    }
+
+//    public Date() {
+//
+//    }
+
+
 }

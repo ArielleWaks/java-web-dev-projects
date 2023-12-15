caa0283424d159bf1bba11c7d552a85e5dca678e
ArielleWaks <ariellemwaks@gmail.com>
2023-12-15 14:53:02 -0600

    Car class addGas method added, exception added, tests added and passed

--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -13,19 +13,28 @@ class CarTest {
         test_car = new Car("Toyota", "Prius", 10, 50);
     }
 
-    //TODO: add emptyTest so we can configure our runtime environment
-    @Test
-    public void emptyTest() {
-        assertEquals(10, 10, .001);
-    }
 
-    //TODO: constructor sets gasTankLevel properly
+    //constructor sets gasTankLevel properly
     @Test
     public void testInitialGasTank() {
         assertEquals( 10, test_car.getGasTankLevel(),.001);
     }
 
-    //TODO: gasTankLevel is accurate after driving within tank range
-    //TODO: gasTankLevel is accurate after attempting to drive past tank range
-    //TODO: can't have more gas than tank size, expect an exception
+    //gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testTankLevelAfterDrive() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+    //gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testTankLevelOverDriveRange() {
+        test_car.drive(550);
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
+    //can't have more gas than tank size, expect an exception
+    @Test
+    public void testGasOverfillException() {
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5), "Car cannot have more gas in tank than the size of the tank");
+    }
 }

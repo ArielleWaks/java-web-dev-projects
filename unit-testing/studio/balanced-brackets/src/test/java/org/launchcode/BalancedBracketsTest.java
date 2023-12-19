1d33edcad3a79e1088c36b548f00bbe18346e0f9
ArielleWaks <ariellemwaks@gmail.com>
2023-12-18 19:56:54 -0600

    tests added; code changed to pass tests

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,6 +7,69 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test string"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void singleBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void textAndBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test[]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[test]"));
+    }
+
+    @Test
+    public void reversedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyOpenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void onlyClosedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
+    @Test
+    public void oddBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void unorderedEvenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]][[]"));
+    }
+
+    @Test
+    public void unbalancedEvenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]"));
+    }
+
+    @Test
+    public void nestedBalancedReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][]]"));
+    }
+
+    @Test
     public void emptyTest() {
         assertEquals(true, true);
     }

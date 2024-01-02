d0cd4fd40dd5ee1b628438a323fb6de245ee6285
ArielleWaks <ariellemwaks@gmail.com>
2024-01-02 12:06:57 -0600

    FlavorComparator changed to sort by number of allergens; ToppingComparator added to sort by name; added to Main

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -8,13 +8,21 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        // Use a Comparator class to sort the 'flavors' array by number of allergens in the 'allergens' array.
         flavors.sort(new FlavorComparator());
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        // Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
         cones.sort(new ConeComparator());
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        // Use a Comparator class to sort the 'toppings' array by 'name' field.
+        toppings.sort(new ToppingComparator());
+
+        // Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("Flavor Options: " + flavors);
+        System.out.println("Cone Options: " + cones);
+        System.out.println("Topping Options: " + toppings);
+
     }
 }

f651f1811eb6afbe11dafe165e90155f30678771
ArielleWaks <ariellemwaks@gmail.com>
2024-01-11 12:05:36 -0600

    methods Divide and CheckFileExtension coded to throw appropriate errors

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,10 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class Main {
+    static Logger Logg = Logger.getLogger("MAIN");
     public static void main(String[] args) {
         // Test out your Divide() function!
 
@@ -10,18 +12,45 @@ public class Main {
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
-        studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Stefanie", null);
 
         // Test out your CheckFileExtension() function!
+        System.out.println(CheckFileExtension(null));
+        System.out.println(CheckFileExtension(""));
+        System.out.println(CheckFileExtension("  "));
+        System.out.println(CheckFileExtension("code.java"));
+        System.out.println(CheckFileExtension("codename"));
+
     }
 
-    public static void Divide(int x, int y)
+    public static double Divide(int x, int y) throws ArithmeticException
     {
-        // Write code here!
+        try {
+            if ( y == 0 ) {
+                throw new ArithmeticException("Possible Points set to zero");
+            }
+            return (double) x /y;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        if (fileName == null || fileName.isEmpty() || fileName.isBlank()) {
+            try {
+                throw new Exception("No file provided");
+            } catch (Exception e) {
+                System.out.println(e);
+                return -1;
+            }
+        }
+        if (fileName.endsWith(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
